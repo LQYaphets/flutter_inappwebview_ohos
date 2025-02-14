8c29884f0a94df93faf94f7e36ef7aa0323859fb
wangtian <wangtian24783@talkweb.com.cn>
2025-02-14 16:07:28 +0800

    实现shouldInterceptRequest拦截请求功能
    Signed-off-by: wangtian <wangtian24783@talkweb.com.cn>

---
+++ flutter_inappwebview_ohos/ohos/src/main/cpp/napi_init.cpp
@@ -0,0 +1,35 @@
+#include "napi/native_api.h"
+#include <uv.h>
+
+static napi_value UvRunNowait(napi_env env, napi_callback_info info)
+{
+    uv_loop_s* loop = nullptr;
+    napi_get_uv_event_loop(env, &loop);
+    uv_run(loop, UV_RUN_NOWAIT);
+    return nullptr;
+}
+
+EXTERN_C_START
+static napi_value Init(napi_env env, napi_value exports) {
+    napi_property_descriptor desc[] = {
+        {"uvRunNowait", nullptr, UvRunNowait, nullptr, nullptr, nullptr, napi_default, nullptr},
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    return exports;
+}
+EXTERN_C_END
+
+static napi_module demoModule = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Init,
+    .nm_modname = "entry",
+    .nm_priv = ((void*)0),
+    .reserved = { 0 },
+};
+
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
+{
+    napi_module_register(&demoModule);
+}
---
+++ flutter_inappwebview_ohos/ohos/src/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,1 @@
+export const uvRunNowait: () => void;
