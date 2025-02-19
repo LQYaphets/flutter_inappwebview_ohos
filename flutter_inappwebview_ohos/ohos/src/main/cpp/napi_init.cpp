a196aba3ebec4e9830de8567a3b36f2d7cd6ff5d
wangtian <wangtian24783@talkweb.com.cn>
2025-02-19 10:56:07 +0800

    添加代码版权头
    Signed-off-by: wangtian <wangtian24783@talkweb.com.cn>

--- flutter_inappwebview_ohos/ohos/src/main/cpp/napi_init.cpp
+++ flutter_inappwebview_ohos/ohos/src/main/cpp/napi_init.cpp
@@ -1,3 +1,18 @@
+/*
+* Copyright (c) 2025 Hunan OpenValley Digital Industry Development Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include "napi/native_api.h"
 #include <uv.h>
 
@@ -10,7 +25,8 @@ static napi_value UvRunNowait(napi_env env, napi_callback_info info)
 }
 
 EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports) {
+static napi_value Init(napi_env env, napi_value exports) 
+{
     napi_property_descriptor desc[] = {
         {"uvRunNowait", nullptr, UvRunNowait, nullptr, nullptr, nullptr, napi_default, nullptr},
     };
